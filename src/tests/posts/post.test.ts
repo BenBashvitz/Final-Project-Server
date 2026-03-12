8b2005dcc76ce3e239853ef4727f7bfc3a8b71ce
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-03-12 19:04:01 +0200

    adding post creation tests

--- src/tests/posts/post.test.ts
+++ src/tests/posts/post.test.ts
@@ -8,7 +8,7 @@ import userModel from "../../models/userModel";
 import { TokenPayload, Tokens } from "../../types/token";
 import { POST_PAGE_SIZE, USERS } from "../consts";
 import { getUserToken } from "../utils";
-import type { PostInputWithUserId, TestPostPage } from "./types";
+import type { PostInput, PostInputWithUserId, TestPostPage } from "./types";
 import { POSTS } from "./consts";
 
 let app: Express;
@@ -25,10 +25,39 @@ beforeAll(async () => {
   userId = (jwt.decode(userTokens.token) as TokenPayload).userId;
 });
 
+beforeEach(async () => {
+  await postModel.deleteMany();
+});
+
+describe("Create post", () => {
+  test("should create posts", async () => {
+    for (const post of POSTS) {
+      const response = await request(app)
+        .post("/post")
+        // .set("Authorization", `Bearer ${userTokens.token}`)
+        .send(post);
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toMatchObject(post);
+    }
+  });
+
+  it("should fail to create a post with missing required fields", async () => {
+    const incompletePost: Partial<PostInput> = {
+      imgUrl: POSTS[0].imgUrl,
+    };
+
+    const response = await request(app)
+      .post("/post")
+      .send(incompletePost)
+      .set("Authorization", `Bearer ${userTokens.token}`);
+
+    expect(response.status).toBe(500);
+  });
+});
+
 describe("with post creation", () => {
   beforeEach(async () => {
-    await postModel.deleteMany();
-
     const postToInsert: PostInputWithUserId[] = POSTS.map((post) => ({
       ...post,
       userId,
