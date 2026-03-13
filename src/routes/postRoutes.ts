16bb92fe524dd67f90d4f7166703afa65a9b8e44
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-03-13 16:21:40 +0200

    change back to old object type qp for cursor in swagger doc

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -13,11 +13,15 @@ const router = express.Router();
  *         name: cursor
  *         description: Cursor for paginating through posts. Omit for the first page.
  *         schema:
- *           type: string
- *           description: >
- *             Cursor for paginating through posts, encoded as a JSON string. Omit for the first page.
- *             The JSON object should have the shape:
- *             { "_id": "string", "creationDate": "date-time string" }.
+ *           type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *               description: ID of the last post in the current page, used as a reference for fetching the next page.
+ *             creationDate:
+ *               type: string
+ *               format: date-time
+ *               description: Creation date of the last post in the current page, used as a reference for fetching the next page.
  *     responses:
  *       200:
  *         description: Paginated list of posts
