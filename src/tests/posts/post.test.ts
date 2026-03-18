20ffe6f0229d554e57a0817ac2c48011a4e4d324
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-03-18 23:05:18 +0200

    fixing post test by getting the rifght fied of token

--- src/tests/posts/post.test.ts
+++ src/tests/posts/post.test.ts
@@ -22,7 +22,7 @@ beforeAll(async () => {
   await userModel.deleteMany();
 
   userTokens = await getUserToken(app, USERS[0]);
-  userId = (jwt.decode(userTokens.token) as TokenPayload).userId;
+  userId = (jwt.decode(userTokens.accessToken) as TokenPayload).userId;
 });
 
 describe("with post creation", () => {
