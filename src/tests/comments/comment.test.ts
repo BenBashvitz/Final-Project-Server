1db46bca08fe8490f7e632840cce09a6468ffb42
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-03-23 18:34:52 +0200

    adding test for fet comments

--- src/tests/comments/comment.test.ts
+++ src/tests/comments/comment.test.ts
@@ -2,15 +2,19 @@ import { Express } from "express";
 import mongoose from "mongoose";
 import request from "supertest";
 import initApp from "../../index";
+import commentModel from "../../models/commentModel";
 import postModel from "../../models/postModel";
 import type { Tokens } from "../../types/token";
+import { User } from "../../types/user";
 import { POSTS } from "../posts/consts";
 import { getCookieSetters, setupMultipleUsersForTests } from "../utils";
 import { COMMENTS } from "./consts";
+import { CommentInput, CommentInputWithUserAndPostIds } from "./types";
 
 let app: Express;
 let userTokens: Tokens[] = [];
 let postIds: string[];
+let userIds: string[];
 
 beforeAll(async () => {
   app = await initApp();
@@ -18,7 +22,7 @@ beforeAll(async () => {
 
   const userData = await setupMultipleUsersForTests(app);
   userTokens = userData.userTokens;
-  const userIds = userData.userIds;
+  userIds = userData.userIds;
 
   const postsWithUserId = POSTS.map((post, index) => ({
     ...post,
@@ -27,10 +31,6 @@ beforeAll(async () => {
 
   const posts = await postModel.create(postsWithUserId);
   postIds = posts.map((post) => post._id.toString());
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
 });
 
 describe("Create comment", () => {
@@ -61,4 +61,57 @@ describe("Create comment", () => {
 
     expect(response.status).toBe(400);
   });
+});
+
+describe("With comments creation", () => {
+  beforeEach(async () => {
+    const commentsToInsert: CommentInputWithUserAndPostIds[] = COMMENTS.map(
+      (comment, index) => ({
+        ...comment,
+        postId: postIds[index],
+        userId: userIds[index],
+      }),
+    );
+
+    await commentModel.create(commentsToInsert);
+  });
+
+  describe("Get comments for a post", () => {
+    it("should get comments for a specific post", async () => {
+      const response = await request(app).get(`/post/${postIds[0]}/comments`);
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body[0]).toHaveProperty("message", COMMENTS[0].message);
+      expect(response.body[0]).toHaveProperty<
+        CommentInputWithUserAndPostIds["creationDate"]
+      >("creationDate", COMMENTS[0].creationDate);
+      expect(response.body[0]).toHaveProperty<
+        CommentInputWithUserAndPostIds["postId"]
+      >("postId", postIds[0]);
+      expect(response.body[0]).toHaveProperty<
+        CommentInputWithUserAndPostIds["userId"]
+      >("user._id", userIds[0]);
+    });
+
+    it("should return 404 for non-existent post", async () => {
+      const nonExistentPostId = new mongoose.Types.ObjectId().toString();
+      const response = await request(app).get(
+        `/post/${nonExistentPostId}/comments`,
+      );
+      expect(response.status).toBe(404);
+    });
+
+    it("should return 400 for invalid post ID", async () => {
+      const invalidPostId = "invalid-id";
+      const response = await request(app).get(
+        `/post/${invalidPostId}/comments`,
+      );
+      expect(response.status).toBe(400);
+    });
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
--- src/tests/comments/types.ts
+++ src/tests/comments/types.ts
@@ -2,3 +2,8 @@ export type CommentInput = {
   message: string;
   creationDate: string;
 };
+
+export type CommentInputWithUserAndPostIds = CommentInput & {
+  userId: string;
+  postId: string;
+};
