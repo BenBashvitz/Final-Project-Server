7b3e6e7dff5d90a0ed9ccbeea25d56a840875e84
ponda <bashvitzben@gmail.com>
2026-03-25 11:27:31 +0200

    fix: user controller use base controller functions

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,26 +1,6 @@
 import userModel from "../models/userModel";
 import BaseController from "./baseController";
-import {RawUser} from "../types/user";
-import {AuthRequest} from "../types/request";
-import {Response} from 'express';
-import {UserIdSchema} from "../schemas/user";
 
-class UserController extends BaseController<RawUser> {
-    constructor() {
-        super(userModel);
-    }
+const userController = new BaseController(userModel);
 
-    override async getById(req: AuthRequest, res: Response) {
-        const { id } = UserIdSchema.parse(req.params);
-
-        const user = await this.model.findById(id);
-
-        if(!user) {
-            return res.status(404).send(`The user was not found`);
-        }
-
-        return super.getById(req, res);
-    }
-}
-
-export default new UserController();
+export default userController
