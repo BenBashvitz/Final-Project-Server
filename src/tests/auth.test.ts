513bc29a11502021c2d61ce4434aadd2bf8d847b
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-03-25 20:47:20 +0200

    adding tests for 403 post and comment and auth tests for post and comment

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -3,14 +3,16 @@ import mongoose from "mongoose";
 import request from "supertest";
 import { refreshTokenCookieName } from "../consts";
 import initApp from "../index";
+import postModel from "../models/postModel";
 import userModel from "../models/userModel";
 import type { Tokens } from "../types/token";
-import { USERS } from "./consts";
+import { COMMENTS, POSTS, USERS } from "./consts";
 import {
   expectNoTokens,
   expectTokens,
   getCookieSetters,
   getTokensFromResponse,
+  setupMultiplePostsForTests,
   setupMultipleUsersForTests,
 } from "./utils";
 
@@ -152,6 +154,108 @@ describe("User logout", () => {
       .set("Cookie", getCookieSetters(userTokens[0]));
 
     expect(response.status).toBe(500);
+  });
+});
+
+describe("Operations with accesses token", () => {
+  let userTokens: Tokens[] = [];
+  let userIds: string[] = [];
+
+  beforeEach(async () => {
+    const userData = await setupMultipleUsersForTests(app);
+    userTokens = userData.userTokens;
+    userIds = userData.userIds;
+  });
+
+  describe("Post", () => {
+    test("should fail to create a post without a token", async () => {
+      const response = await request(app).post("/post").send(POSTS[0]);
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    test("should fail to create a post with invalid token", async () => {
+      const invalidToken = userTokens[0].accessToken + "a";
+
+      const response = await request(app)
+        .post("/post")
+        .set(
+          "Cookie",
+          getCookieSetters({
+            accessToken: invalidToken,
+            refreshToken: userTokens[0].refreshToken,
+          }),
+        )
+        .send(POSTS[0]);
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    test("should fail to create a post with expired token", async () => {
+      await new Promise((r) => setTimeout(r, 5000));
+
+      const response = await request(app)
+        .post("/post")
+        .set("Cookie", getCookieSetters(userTokens[0]))
+        .send(POSTS[0]);
+
+      expect(response.statusCode).toBe(401);
+    }, 10000);
+  });
+
+  describe("Comment", () => {
+    let postId: string;
+
+    beforeEach(async () => {
+      await postModel.deleteMany();
+
+      postId = await setupMultiplePostsForTests(userIds);
+    });
+
+    test("should fail to create a comment without a token", async () => {
+      const response = await request(app)
+        .post(`/post/${postId}/comment`)
+        .send({
+          ...COMMENTS[0],
+          postId,
+        });
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    test("should fail to create a comment with invalid token", async () => {
+      const invalidToken = userTokens[0].accessToken + "a";
+
+      const response = await request(app)
+        .post(`/post/${postId}/comment`)
+        .set(
+          "Cookie",
+          getCookieSetters({
+            accessToken: invalidToken,
+            refreshToken: userTokens[0].refreshToken,
+          }),
+        )
+        .send({
+          ...COMMENTS[0],
+          postId,
+        });
+
+      expect(response.statusCode).toBe(401);
+    });
+
+    test("should fail to create a comment with expired token", async () => {
+      await new Promise((r) => setTimeout(r, 5000));
+
+      const response = await request(app)
+        .post(`/post/${postId}/comment`)
+        .set("Cookie", getCookieSetters(userTokens[0]))
+        .send({
+          ...COMMENTS[0],
+          postId,
+        });
+
+      expect(response.statusCode).toBe(401);
+    }, 10000);
   });
 });
 
--- src/tests/consts.ts
+++ src/tests/consts.ts
@@ -1,4 +1,5 @@
 import type { UserInput } from "../types/user";
+import type { CommentInput, PostInput } from "./types";
 
 export const USERS: UserInput[] = [
   {
@@ -20,5 +21,42 @@ export const USERS: UserInput[] = [
     email: "example4@example.com",
     username: "example4User",
     password: "example4Pass",
+  },
+];
+
+const currentTime = Date.now();
+const DAY_MS = 24 * 60 * 60 * 1000;
+
+export const POSTS: PostInput[] = [
+  {
+    description: "Test Description1",
+    imgUrl: "https://example.com/image.jpg",
+    creationDate: new Date(currentTime).toISOString(),
+  },
+  {
+    description: "Another Description2",
+    imgUrl: "https://example.com/image2.jpg",
+    creationDate: new Date(currentTime - 1 * DAY_MS).toISOString(),
+  },
+  {
+    description: "Test Description3",
+    imgUrl: "https://example.com/image3.jpg",
+    creationDate: new Date(currentTime - 2 * DAY_MS).toISOString(),
+  },
+  {
+    description: "Another Description4",
+    imgUrl: "https://example.com/image4.jpg",
+    creationDate: new Date(currentTime - 3 * DAY_MS).toISOString(),
+  },
+];
+
+export const COMMENTS: CommentInput[] = [
+  {
+    message: "Test Comment",
+    creationDate: new Date(currentTime).toISOString(),
+  },
+  {
+    message: "Another Comment",
+    creationDate: new Date(currentTime - 1 * DAY_MS).toISOString(),
   },
 ];
