4ab6e8b1f703a262721e9a38cca461a0ba7a2444
ponda <bashvitzben@gmail.com>
2026-04-17 16:03:03 +0300

    fix: pull request

--- src/services/ragChunkService.ts
+++ src/services/ragChunkService.ts
@@ -78,7 +78,7 @@ class RagChunkService {
 
     private generateEmbedding = async (data: string): Promise<number[]> => {
         if (!this.textEmbedder) {
-            this.textEmbedder = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
+            this.textEmbedder = await pipeline('feature-extraction');
         }
 
         const output = await this.textEmbedder(data, {
