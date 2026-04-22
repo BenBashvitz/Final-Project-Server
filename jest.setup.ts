760c4fb50a68fb4b61140ac81dde0b105d2c28e9
Noam Shimoni <noamsh9103@cs.colman.ac.il>
2026-04-22 17:24:01 +0300

    adding tsconfig for build production, exlucding test files and including them in tsconfig

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,15 +2,11 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.test" });
 
 jest.mock("@xenova/transformers", () => ({
-    pipeline: jest.fn().mockResolvedValue(
-        jest.fn().mockImplementation((data: string) => {
-            return Promise.resolve({
-                data: [0.1, 0.2, 0.3, 0.4, 0.5],
-            });
-        })
-    ),
+  pipeline: jest.fn().mockResolvedValue(
+    jest.fn().mockImplementation((data: string) => {
+      return Promise.resolve({
+        data: [0.1, 0.2, 0.3, 0.4, 0.5],
+      });
+    }),
+  ),
 }));
-
-export default {
-    setupFiles: ["./jest.setup.ts"],
-}
