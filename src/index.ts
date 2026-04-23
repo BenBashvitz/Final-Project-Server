cdaff5542c5bd20f7fe7d3bfab9f48a41b8c8fe1
ponda <bashvitzben@gmail.com>
2026-04-23 22:32:00 +0300

    feat: serve react static files from the dist folder

--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,54 @@ import express from "express";
 import mongoose from "mongoose";
 import swaggerUi from "swagger-ui-express";
 import config from "./configs/envVar";
-import { UPLOADS_ROUTE } from "./consts";
+import {UPLOADS_ROUTE} from "./consts";
 import authRouter from "./routes/authRoutes";
 import likeRouter from "./routes/likeRoutes";
 import multerRouter from "./routes/multerRoutes";
 import postRouter from "./routes/postRoutes";
 import swaggerSpec from "./swagger";
 import userRoutes from "./routes/userRoutes";
+import path from "path";
 
 const initApp = async () => {
-  const app = express();
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(
-    cors({
-      origin: config.CLIENT_URL,
-      credentials: true,
-    }),
-  );
+    const app = express();
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(
+        cors({
+            origin: config.CLIENT_URL,
+            credentials: true,
+        }),
+    );
 
-  await mongoose.connect(config.MONGODB_URL);
+    await mongoose.connect(config.MONGODB_URL);
 
-  const db = mongoose.connection;
+    const db = mongoose.connection;
 
-  db.on("error", (error) => {
-    console.error("An error occurred while connecting to MongoDB: ", error);
-  });
+    db.on("error", (error) => {
+        console.error("An error occurred while connecting to MongoDB: ", error);
+    });
 
-  db.once("open", () => {
-    console.log("Connected to MongoDB");
-  });
+    db.once("open", () => {
+        console.log("Connected to MongoDB");
+    });
 
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.use("/post", postRouter);
-  app.use("/like", likeRouter);
-  app.use("/auth", authRouter);
-  app.use("/upload", multerRouter);
-  app.use(`/${UPLOADS_ROUTE}`, express.static("public/uploads"));
-  app.use("/auth", authRouter);
-  app.use("/user", userRoutes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use("/post", postRouter);
+    app.use("/like", likeRouter);
+    app.use("/auth", authRouter);
+    app.use("/upload", multerRouter);
+    app.use(`/${UPLOADS_ROUTE}`, express.static("public/uploads"));
+    app.use("/auth", authRouter);
+    app.use("/user", userRoutes);
 
-  return app;
+    app.use(express.static(path.join(__dirname, '..', 'dist')));
+    app.get('/*splat', (req, res) => {
+        console.log(path.join(__dirname, '..', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+    });
+
+    return app;
 };
 
 export default initApp;
